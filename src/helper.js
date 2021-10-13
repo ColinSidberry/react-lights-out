57e68aea25df16484e9e2adb1f5cd275c1af1225
Colin Sidberry <colinsidberry@gmail.com>
2021-10-13 16:52:28 -0700

    actually committting the helper functinos.

--- src/helper.js
+++ src/helper.js
@@ -1,9 +1,19 @@
 /**Takes in width and height and 
  * returns board of specified width and height with undefined in each cell */
 function makeBoard(width, height) {
-    let columns = Array.from({ length: height });
-    let board = columns.map(c => Array.from({ length: width }));
+    let rows = Array.from({ length: height });
+    let board = rows.map(c => Array.from({ length: width }));
     return board;
 }
 
-export {makeBoard};
+
+function determineOnOff(chanceLightStartsOn){
+    const randNum = Math.random();
+    return randNum <= chanceLightStartsOn ? true : false;
+}
+
+export { makeBoard, determineOnOff };
+
+
+        // anything less than prob tru
+        //anything higher is false
