2c4d3cd3649f8f4dc38d4c21e8f42417da50d436
kimberly lin <kimberly.lin10@gmail.com>
2021-10-13 17:17:37 -0700

    wrote flipCellsAround, hasWon in Board.js. UNTESTED. UNSURE IF WORKS

--- src/Board.js
+++ src/Board.js
@@ -29,13 +29,12 @@ import { makeBoard, determineOnOff } from "./helper";
  **/
 
 function Board({ nrows, ncols, chanceLightStartsOn }) {
-  const [board, setBoard] = useState(createBoard());
+  const [board, setBoard] = useState(() => createBoard(nrows, ncols, chanceLightStartsOn));
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
   function createBoard(nrows, ncols, chanceLightStartsOn) {
     let initialBoard = makeBoard(ncols, nrows);
 
-    // TODO: create array-of-arrays of true/false values
     return initialBoard.map(
       row => row.map(
         cell => determineOnOff(chanceLightStartsOn)
@@ -44,26 +43,39 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
   }
 
   function hasWon() {
-    // TODO: check the board in state to determine whether the player has won.
+    const rowsWithLightsOn = board.filter(row => row.includes(true));
+    return rowsWithLightsOn.length === 0;
   }
 
+  /** Takes in coord "y-x" */
   function flipCellsAround(coord) {
     setBoard(oldBoard => {
-      const [y, x] = coord.split("-").map(Number);
+      const [y, x] = coord.split("-").map((strNum) => Number(strNum));
 
       const flipCell = (y, x, boardCopy) => {
         // if this coord is actually on board, flip it
-
         if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
           boardCopy[y][x] = !boardCopy[y][x];
         }
       };
 
       // TODO: Make a (deep) copy of the oldBoard
+      const boardCopy = [...oldBoard];
 
       // TODO: in the copy, flip this cell and the cells around it
+      //flipCell of clicked bulb
+      flipCell(y, x, boardCopy);
+      //flipCell of left of click
+      flipCell(y, x - 1, boardCopy);
+      //flipCell of right of click
+      flipCell(y, x + 1, boardCopy);
+      //flipCell of up of click
+      flipCell(y + 1, x, boardCopy);
+      //flipCell of down of click
+      flipCell(y - 1, x, boardCopy);
 
       // TODO: return the copy
+      return boardCopy;
     });
   }
 
